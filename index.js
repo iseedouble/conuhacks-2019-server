95dad9a32690950f8db665963cbed280770d97f1
Olivia You-Tuon <olivia.you-tuon@polymtl.ca>
2019-01-27 03:59:51 -0500

    Use fs to save photo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 //const tf = require('@tensorflow/tfjs-node');
 // const image = require('get-image-data');
 // const cv = require('');
@@ -24,8 +25,8 @@ app.get('/', (req, res) => res.send('Hello Marcus!'));
 app.get('/classify', (req, res) => res.send('BANANA'));
 
 app.post('/photo', (req, res) => {
-  console.log('Received post: ');
-  console.log(req.body.photo);
+  fs.writeFileSync('out.png', req.body.photo, 'base64', (err) => console.log(err));
+  console.log('Saved.');  
 })
 
 app.listen(port, () => console.log('marcus has started'));
